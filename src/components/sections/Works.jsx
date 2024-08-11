a694b8157ad857bd8c7318288a42bbfecf265f86
kyrylo <kirillulianow@gmail.com>
2024-08-11 22:17:58 +0300

    changed card roundness

--- src/components/sections/Works.jsx
+++ src/components/sections/Works.jsx
@@ -24,7 +24,7 @@ const ProjectCard = ({
 
     return (
         <motion.div variants={fadeIn('up', 'spring', index * 0.5, 0.75)}>
-            <div className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
+            <div className="bg-tertiary p-5 rounded-3xl sm:w-[360px] w-full">
                 <div className="relative w-full h-[230px]">
                     <img
                         src={image}
